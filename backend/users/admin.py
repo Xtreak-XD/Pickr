b672ad925ecab2da94189e40be8321a7c392fb3f
HaruCrisp <haru.hori001@gmail.com>
2025-10-25 12:49:53 -0400

    Clean up Algorithm branch - keep only algorithm and test files

--- backend/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "onboarding_done", "created_at")
-    search_fields = ("user__username", "user__email")
