d3591186aaac2969a8a2844339480f2d97e54660
MiguelA10 <mikeandresavila@gmail.com>
2025-10-25 16:08:51 -0400

    ADded a login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import { useEffect, useState } from 'react'
-import { Card } from "./components/Card"
 import { NavBar } from './components/Navbar'
-import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Profile } from './pages/Profile';
-import { Settings } from './pages/Settings';
-import { Login } from './pages/Login';
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Profile } from './pages/Profile'
+import { Settings } from './pages/Settings'
+import { Login } from './pages/Login'
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-zinc-800">
+    <div className="flex items-center justify-center min-h-screen shadow-[5px_0px_10px_rgba(100,24,0,0.3)] bg-zinc-800">
       {/* App container */}
-      <div className="flex flex-col items-center justify-between min-h-screen w-64 sm:w-80 md:w-96 lg:w-[28rem] bg-white shadow-lg rounded-2xl">
+      <div className="flex flex-col items-center justify-between min-h-screen w-64 sm:w-80 md:w-96 lg:w-[28rem] bg-[#F0A500] shadow-lg rounded-2xl" >
         <div className="flex-1 flex items-center justify-center">
           <Routes>
-            <Route path={"/login"} component={Login}></Route>
+            <Route path={"/login"} element={<Login/>}></Route>
             <Route path={"/"} element={<Home/>} />
             <Route path={"/settings"} element={<Settings/>} />
             <Route path={"/profile"} element={<Profile/>} />
