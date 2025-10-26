9920e2bb09d9ef88d69400aeb8cc27e0e85b2777
CR1SI <cristianpmason@icloud.com>
2025-10-26 10:57:57 -0400

    s

--- backend/liftoff/liftoff/settings.py
+++ backend/liftoff/liftoff/settings.py
@@ -102,16 +102,16 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ["PGDATABASE"],
+#         'USER': os.environ["PGUSER"],
+#         'PASSWORD': os.environ["PGPASSWORD"],
+#         'HOST': os.environ["PGHOST"],
+#         'PORT': os.environ["PGPORT"],
+#     }
+# }
 
 
 # Password validation
